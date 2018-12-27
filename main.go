3b82e36cea00b0dfe0c260f08d6c4ae213bf291f
yushan <yusankurban@gmail.com>
2018-12-27 19:14:52 +0800

    test prometheus

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"git.yusank.space/yusank/klyn"
 	"git.yusank.space/yusank/klyn-log"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // Logger - global logger
 var Logger klynlog.Logger
 
 func main() {
+	go func() {
+		log.Println("start")
+		http.Handle("/metrics", promhttp.Handler())
+		log.Println(http.ListenAndServe(":8080", nil))
+	}()
+
 	core := klyn.Default()
 	core.UseMiddleware(middleBefore, middleAfter)
 	root := core.Group("")
