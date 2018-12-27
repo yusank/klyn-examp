c8a02043ef1d23d8ee7f3d3c65ee1cbb47ba0d64
yushan <yusankurban@gmail.com>
2018-12-27 21:36:29 +0800

    add etcd test

---
+++ etcd/agent.go
@@ -0,0 +1,73 @@
+package etcd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+
+	"go.etcd.io/etcd/client"
+)
+
+type Server struct {
+	info   Serverinfo // 服务器信息
+	Status int        // 服务器状态
+}
+
+// all server
+type serverPool struct {
+	services map[string]*Server // server 列表
+	client   client.Client
+	mu       sync.RWMutex
+}
+
+var (
+	DefaultPool serverPool
+	once        sync.Once
+)
+
+func Init(endpoints []string) {
+	once.Do(func() { DefaultPool.init(endpoints) })
+}
+
+func (p *serverPool) init(hosts []string) {
+	// init etcd client
+	cfg := client.Config{
+		Endpoints: hosts, //
+		Transport: client.DefaultTransport,
+	}
+	etcdcli, err := client.New(cfg)
+	if err != nil {
+		log.Panic(err)
+	}
+	p.client = etcdcli
+	// init
+	p.services = make(map[string]*Server)
+
+	go p.watcher()
+}
+
+// watcher for data change in etcd directory
+func (p *serverPool) watcher() error {
+	kAPI := client.NewKeysAPI(p.client)
+	w := kAPI.Watcher("lc_server/", &client.WatcherOptions{Recursive: true})
+	// 监听 "lc_server/" 当 "lc_server/" 子目录改变时能收到通知
+	for {
+		resp, err := w.Next(context.Background())
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		if resp.Node.Dir {
+			continue
+		}
+		switch resp.Action {
+		case "set", "create", "update", "compareAndSwap":
+			fmt.Println(resp.Action)
+			// 添加或更新 server 信息
+		case "delete", "compareAndDelete", "expire":
+			fmt.Println(resp.Action)
+			// 当过期之后删除 server
+		}
+	}
+}
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 
+	"git.yusank.space/yusank/klyn-examp/etcd"
+
 	"git.yusank.space/yusank/klyn"
 	"git.yusank.space/yusank/klyn-log"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -18,6 +20,11 @@ func main() {
 		http.Handle("/metrics", promhttp.Handler())
 		log.Println(http.ListenAndServe(":8080", nil))
 	}()
+
+	etcd.Init([]string{"http://127.0.0.1:2379"})
+	etcd.RegisterClient(1, "127.0.0.1", []string{"http://127.0.0.1:2379"})
+	etcd.RegisterClient(2, "127.0.0.1", []string{"http://127.0.0.1:22379"})
+	etcd.RegisterClient(3, "127.0.0.1", []string{"http://127.0.0.1:32379"})
 
 	core := klyn.Default()
 	core.UseMiddleware(middleBefore, middleAfter)
